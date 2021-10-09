b133f33975a76bee2e0b7ba8150baa11f55bdf5a
Harsh R Jain <rjainharsh@gmail.com>
2021-10-10 03:46:18 +0530

    ADDED: website that allows user to enter a valid name

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+const salary = document.querySelector('#salary');
+const salary_output = document.querySelector('.salary-output');
+salary_output.textContent = salary.value;
+salary.addEventListener('input',function() {
+    salary_output.textContent = salary.value;
+});
+
+const text = document.querySelector('#text');
+const text_error = document.querySelector('.text-error');
+text.addEventListener('input', function() {
+    let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+    if(nameRegex.test(text.value))
+        text_error.textContent = "";
+    else text_error.textContent = "Name is incorrect!"
+})
